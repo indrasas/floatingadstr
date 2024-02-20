80fcd268e4d7db67d5762b9ffef22347e50ece7d
Insrasas <137709253+indrasas@users.noreply.github.com>
2024-02-21 00:37:19 +0800

    Create firsttier.js

---
+++ firsttier.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '18fad771288701b1e919588761884b12',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//cockysnailleather.com/18fad771288701b1e919588761884b12/invoke.js"></script>
